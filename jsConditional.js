03fd809e1553e53505c41353926d242d4a502801
Arpit Gupta <78097741+arpitgupta630@users.noreply.github.com>
2022-05-20 08:31:57 +0530

    condition statement completed

--- jsConditional.js
+++ jsConditional.js
@@ -41,3 +41,33 @@ if(userGuess === winningNumber){
 }else if(userGuess>winningNumber){
   console.log('Too High');
 }
+
+// Switch Statment
+let day = 2;
+
+switch(day){
+  case 0:                       // case check value of day=0
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log('Monday');
+    break;
+  case 2:
+    console.log('Tuesday');
+    break;
+  case 3:
+    console.log('Wednesday');
+    break;
+  case 4:
+    console.log('Thrusday');
+    break;
+  case 5:
+    console.log('Friday');
+    break;
+  case 6:
+    console.log('Saturday');
+    break;
+  default:
+    console.log('Invalid Day');
+    break;
+}
