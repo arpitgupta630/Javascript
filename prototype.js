4b2e36f247a0056047ecd9370f6eef5eb7ce67d4
Arpit Gupta <78097741+arpitgupta630@users.noreply.github.com>
2022-06-17 17:46:40 +0530

    JS Function is Object

--- prototype.js
+++ prototype.js
@@ -4,5 +4,14 @@ function hello(){
 
 //! Javascript function ===> is both a function and an Object.
 
-// we can access properties in function like we access them in objects.
-console.log(hello.name);
+//? we can access properties in function like we access them in objects.
+// console.log(hello.name);         // name property tells name of function 
+
+//? we can add our own properties in functions:
+// hello.myOwnProperty = 'I created this property';
+// console.log(hello.myOwnProperty);
+
+//? function provide more useful properties like: call() | apply() | bind()
+
+//! Prototype: function provide empty object {}. We can access them through prototype.
+console.log(hello.prototype);
