2d2c498f4803d65903dcc6966ee08530275cf0ea
Arpit Gupta <78097741+arpitgupta630@users.noreply.github.com>
2022-06-17 17:36:01 +0530

    Created prototype.js

---
+++ prototype.js
@@ -0,0 +1,8 @@
+function hello(){
+    console.log('Hello World!!');
+};
+
+//! Javascript function ===> is both a function and an Object.
+
+// we can access properties in function like we access them in objects.
+console.log(hello.name);
