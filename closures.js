b186ad423ac2661c5880db7a4ec284d510af895f
Arpit Gupta <78097741+arpitgupta630@users.noreply.github.com>
2022-06-24 00:19:28 +0530

    Clousers

--- closures.js
+++ closures.js
@@ -1,6 +1,6 @@
 //! Closures: function returning function
 
-// Example 1
+//! Example 1
 console.log('\nExample 1:');
 function fullName(firstName, lastName){
     function printName(){
@@ -12,7 +12,7 @@ function fullName(firstName, lastName){
 const ans = fullName('Arpit', 'Gupta');
 ans();
 
-// Example 2
+//! Example 2
 console.log('\nExample 2:');
 function hello(x){
     const a = 'Var A';
@@ -24,19 +24,39 @@ function hello(x){
 const ex2 = hello('args')
 ex2()
 
-//Example 3
+//!Example 3
 console.log('\nExample 3:');
 // function power(x){
-//     return function(num){
-//         return num**x;
+    //     return function(number){
+//         return number**x;
 //     };
 // };               
 
 //! This can be written as below in arrow
 
-const power = x => num => num**x;       // Arrow function
+const power = x => number => number**x;       // Arrow function
 
 const square = power(2);
 console.log(square(3));
 const cube = power(3);
 console.log(cube(2));
+
+//! Example 4: function call counter
+console.log('\nExample 4:');
+function myFuncn(){
+    let counter = 0;
+    return function(){
+        if(counter < 1){
+            console.log('Hello User');
+            counter++;
+        } else {
+            console.log('Already Called');
+        };
+    };
+};
+const user1 = myFuncn();
+user1();
+user1();
+const user2 = myFuncn();
+user2();
+user2();
